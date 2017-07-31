68c3c2e2177f69689d40f9b28172336a4d32e860
Aaron Gottlieb <atticus19@gmail.com>
2017-07-31 08:32:31 -0600

    adds catch statement to handle errors from weather axios calls

--- app-promise.js
+++ app-promise.js
@@ -24,10 +24,22 @@ const BASE_WEATHER_ADDRESS = 'https://api.darksky.net/forecast/620d098c7e12b72c8
 
 axios.get(`${BASE_MAPS_URL}${encodedAddress}`)
   .then(response => {
+    if(response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Unable to find that address')
+    }
     const { lat, lng } =
      response.data.results[0].geometry.location;
+     console.log(response.data.results[0].formatted_address)
     return axios.get(`${BASE_WEATHER_ADDRESS}${lat},${lng}`);
   })
   .then(response => {
-    console.log(response.data.currently.temperature)
+    const { temperature, apparentTemperature } = response.data.currently;
+    console.log(`The current temp is ${temperature}, it feels like ${apparentTemperature}`)
+  })
+  .catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+      console.log('Unable to connect ot API servers.');
+    } else {
+      console.log(e.message);
+    }
   })
